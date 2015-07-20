c7cee6dac0237efcdd1cab292266a7b780fe9280
Robert Sullivan <r.sullivan@nature.com>
2015-07-20 15:24:40 +0100

    Create graph, output as json, display with d3.

--- reference_graph/build_graph.py
+++ reference_graph/build_graph.py
@@ -1,16 +1,17 @@
+from bson import json_util
 from itertools import zip_longest
+import json
 
 from source_ref import *
 
 import networkx as nx
+from networkx.readwrite import json_graph
 import pylab
 
 """
-use ipython --matplotlib
+use ipython 
 
-and import pylab
-
-HOWTO: http://matplotlib.org/faq/installing_faq.html#windows-installer
+I need to know titles and pmids of all articles at the very least.
 
 """
 
@@ -55,5 +56,55 @@ for ref in seed_ancestor_references_list:
 
 MDG.add_edges_from(ancestor_edges)
 
-nx.draw(MDG, pos=nx.spring_layout(MDG)) # a mess
+#nx.draw(MDG, pos=nx.spring_layout(MDG)) # a mess
 
+"""
+Find out degrees
+Out degrees represent edges going out and represent "cited_by"
+In edges represent "cites" relationship
+
+"""
+
+out_degrees = MDG.out_degrees()
+
+highest_out_degrees = sorted(out_degrees.values(), reverse=True)
+
+highest_out_degrees[:5]
+
+
+"""
+Find all_shorteset paths from source to target
+source is one of highest_out_degree nodes
+target is seed_node
+
+Look up keys that have a value
+
+store shortest paths in a list 
+
+"""
+shortest_paths = []
+
+for key, value in out_degrees.items():
+	if value in highest_out_degrees[:5]:
+		shortest_paths.append(nx.all_shortest_paths(MDG, source=key, target=seed_node['_id']))
+		
+
+
+
+
+"""
+Select subgraph from graph using source and target
+
+"""
+
+"""
+Output graph to json for d3.js
+
+"""
+
+
+
+data = json_graph.node_link_data(MDG)
+
+with open('data.json', 'w') as f:
+	json.dump(data, f, default=json_util.default)
