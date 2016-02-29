bd03301eefeb5b3eb8d3dea34124248dd55f6a62
Robert Sullivan <robertjsullivan.esq@gmail.com>
2016-02-29 22:57:23 +0000

    Created models.py ready for data migration.@

--- referencegraph/models.py
+++ referencegraph/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+class Journal(models.Model):
+	title = models.CharField(max_length=100)
+	doi_prefix = models.CharField(max_length=50)
+
 class Article(models.Model):
+	article_id = models.IntegerField(primary_key=True)
 	title = models.CharField(max_length=500)
 	pmid = models.CharField(max_length=50)
-
-
-class Author(models.Model):
 	name = models.CharField(max_length=100)
+	journal = models.ForeignKey(Journal)
+	article_reference_id = models.IntegerField()
 
-
-class Journal(models.Model):
-	pass
 
 
 class ArticleReferences(models.Model):
-	pass
+	article_reference_id = models.ForeignKey(Article)
+	article_id = models.ForeignKey(Article)
